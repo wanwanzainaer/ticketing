f74fc9e058e2cbce0d639cfe588564fc1b68a6dd
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-10-17 22:55:45 -0700

    for the commit

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,7 +3,7 @@ import buildClient from '../api/build-client';
 const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You are sign in</h1> : <h1>You are not sign in</h1>;
 };
-
+//for commit
 LandingPage.getInitialProps = async (context) => {
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
