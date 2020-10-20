0fe9cdc1816d79bbc28d4e9670550b90c1892df2
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-10-19 22:58:24 -0700

    Add the test auth JWT

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@hsctickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@hsctickets/common';
 
 import { createTicketRouter } from './routes/new';
 const app = express();
@@ -14,6 +14,7 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 app.use(createTicketRouter);
 app.all('*', async (req, res) => {
   throw new NotFoundError();
