83bc2828df1ef2ce910d911be0934a1dbd39e6de
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-10-30 15:04:15 -0700

    Add new common and add new publisher and publisher event

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,28 +1,28 @@
 import nats from 'node-nats-streaming';
-
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 // stan or client
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 console.clear();
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
   stan.on('close', () => {
     console.log('NATS connection closed!!!');
     process.exit();
   });
-
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
-
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: 'jisdfjsdif',
+      title: 'Test',
+      price: 30,
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 process.on('SIGINT', () => stan.close());
