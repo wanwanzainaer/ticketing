668523dbb5433ab975b52e86898d403d20f92dad
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-03 13:32:50 -0800

    Add index functional with index router and index test

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -19,7 +19,7 @@ interface TicketModel extends mongoose.Model<TicketDoc> {
 const ticketSchema = new mongoose.Schema(
   {
     title: {
-      tpye: String,
+      type: String,
       required: true,
     },
     price: {
