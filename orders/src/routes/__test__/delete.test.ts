8cb3cabbf0949ff56c02c96783461bf897f15cd9
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-04 23:18:00 -0800

    Add created and cancelled event and its publisher

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('marks the order as cancelled', async () => {
   // Create a ticket
@@ -34,4 +35,29 @@ it('marks the order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('emits a order cancelled event');
+it('emits a order cancelled event', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // make a request to build an order with this ticket
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,12 +5,14 @@ import {
 } from '@hsctickets/common';
 import express, { Request, Response } from 'express';
 import { Order } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
   const { orderId } = req.params;
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate('ticket');
   if (!order) throw new NotFoundError();
   if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
 
@@ -18,7 +20,12 @@ router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
   await order.save();
 
   // Publishing an event saying this was cancelled
-
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id,
+    },
+  });
   res.status(204).send(order);
 });
 
